f8e510fed6666ef044c333957347cf5b970bc4eb
Fatima Achimugu <fatima.achims@gmail.com>
2024-07-21 20:52:06 +0100

    add small script

--- admin/script.js
+++ admin/script.js
@@ -19,4 +19,24 @@ $(document).ready(function() {
     $('body').toggleClass('dark-theme-variables');
     $themeToggler.find('span').toggleClass('active');
   });
+
+  // // Get the modal
+  // let $modal = $('#myModal');
+  $('#addBtn').on('click', function(){
+    $('#myModal').show();
+  });
+  
+  //close modal
+  $('#cancel').on('click', function(){
+    $('#myModal').hide();
+  })
+ 
+
+  // When the user clicks anywhere outside of the modal, close it
+  $(window).on('click', function(event) {
+    if ($(event.target).is($modal)) {
+      $modal.hide();
+    }
+  });
+
 });
