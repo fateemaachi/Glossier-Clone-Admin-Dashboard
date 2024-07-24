52fc897fb7a180439d8e44539643b9ca1117d5dd
Firdausi1 <firdausib18@gmail.com>
2024-07-24 11:04:37 +0100

    Edit category

--- admin/js/category.js
+++ admin/js/category.js
@@ -1,37 +1,57 @@
-$(document).ready(function() {
+$(document).ready(function () {
   const baseURL = "http://ecommerce.reworkstaging.name.ng/v2";
-  const merchant = JSON.parse(localStorage.getItem('merchantDetails'));
+  const merchant = JSON.parse(localStorage.getItem("merchantDetails"));
   let valid = false;
   const modal = $("#myModal");
+  let categoryId = "";
+  let categories = [];
 
-  function validateFields (data) {
-    if(data.name === "" && data.image === ""){
+  function validateFields(data) {
+    if (data.name === "" && data.image === "") {
       valid = false;
     } else {
       valid = true;
     }
   }
-  $("#addCat").on('click', function(){
+  $("#addCat").on("click", function () {
     const data = {
       merchant_id: merchant.id,
       name: $("#catName").val().trim().toLowerCase(),
       image: $("#catImage").val().trim().toLowerCase(),
-    }
+    };
     validateFields(data);
 
-    if(valid) {
-      $.ajax({
-        url:`${baseURL}/categories`,
-        method: "POST",
-        data: data,
-        success: function(){
-          alert("Category added successfully")
-          modal.hide()
-          getCategories()
-        }
-      })
+    if (valid) {
+      if ($("#addCat").text() === "Update Category" && categoryId !== "") {
+        $.ajax({
+          url: `${baseURL}/categories/${categoryId}`,
+          method: "PUT",
+          data: data,
+          success: function (response) {
+            console.log(response);
+            alert("Category updated successfully");
+            modal.hide();
+            getCategories();
+            $("#catName").val("");
+            $("#catImage").val("");
+            categoryId = "";
+            $("#addCat").html("Add Category");
+          },
+        });
+      } else {
+        $.ajax({
+          url: `${baseURL}/categories`,
+          method: "POST",
+          data: data,
+          success: function () {
+            alert("Category added successfully");
+            modal.hide();
+            getCategories();
+          },
+        });
+      }
     }
-  })
+  });
 
   function getCategories() {
     $.ajax({
@@ -39,21 +59,34 @@ $(document).ready(function() {
       method: "GET",
       success: function (response) {
         displayCategories(response);
-        console.log(response)
+        categories = response;
       },
     });
   }
-  getCategories()
+  getCategories();
 
   function displayCategories(categ) {
     const display = $("#categoryTable");
     display.empty();
 
     categ.map((cat) => {
-      display.append(`<tr>
+      display.append(`<tr data-id="${cat.id}" class="catItem">
       <td class="flex justify-center"><img src="${cat.image}" alt="${cat.name}" class="catImage"></td>
       <td class="primary">${cat.name}</td>
     </tr>`);
     });
   }
-})
+
+  $(document).on("click", ".catItem", function () {
+    const id = $(this).data("id");
+
+    const categoryExists = categories.find((item) => item.id === id);
+    if (categoryExists) {
+      $("#catName").val(categoryExists.name);
+      $("#catImage").val(categoryExists.image);
+      modal.show();
+      categoryId = id;
+      $("#addCat").html("Update Category");
+    }
+  });
+});
